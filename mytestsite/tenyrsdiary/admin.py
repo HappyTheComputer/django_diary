11aa18ace0d2c9a941eb462ad3b079c77c6b8c11
HappyTheComputer <happythecomputer@gmail.com>
2020-08-28 17:18:51 +0800

    create diary database type

--- mytestsite/tenyrsdiary/admin.py
+++ mytestsite/tenyrsdiary/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Diary
+from .models import DayInYesr, Diary, TodoList, BirthDay, MemorialDay, Asset, Family, Suprise, Relationship, Study, Career, Mind
 
 # Register your models here.
+class BirthInline(admin.TabularInline):
+    model = BirthDay
+    extra = 1
+
+class MemorialInline(admin.TabularInline):
+    model = MemorialDay
+    extra = 1
+
+class DairyYrInline(admin.TabularInline):
+    model = Diary
+    extra = 1
+
+class DayAdmin(admin.ModelAdmin):
+    inlines = [BirthInline, MemorialInline, DairyYrInline] 
+
+admin.site.register(DayInYesr, DayAdmin)
 admin.site.register(Diary)
--- mytestsite/tenyrsdiary/models.py
+++ mytestsite/tenyrsdiary/models.py
@@ -1,19 +1,60 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class DayInYesr(models.Model):
+    dayKey = models.CharField("日期", max_length=10, unique=True, primary_key=True)
+
 class Diary(models.Model):
-    today = models.DateTimeField()
-    birth = models.CharField(max_length=100)
-    weather = models.CharField(max_length=100)
-    #7個主題
-    mind = models.TextField(blank=True)
-    career = models.TextField(blank=True)
-    study = models.TextField(blank=True)
-    relationship = models.TextField(blank=True)
-    asset = models.TextField(blank=True)
-    family = models.TextField(blank=True)
-    suprise = models.TextField(blank=True)
-    #todo清單
-    todo = models.CharField(max_length=100)
-    todo_check = models.BooleanField(default=False)
+    diary_date = models.DateField(unique=True, primary_key=True)
+    dayKey = models.ForeignKey(DayInYesr, on_delete=models.CASCADE)
+
+    def day(self):
+        return diary_date.date().strftime("%m-%d")
     
+    def year(self):
+        return diary_date.date().strftime("%Y")
+        
+class BirthDay(models.Model):
+    dayKey = models.ForeignKey(DayInYesr, on_delete=models.CASCADE)
+    birthName = models.CharField("壽星", max_length=30)
+
+class MemorialDay(models.Model):
+    dayKey = models.ForeignKey(DayInYesr, on_delete=models.CASCADE)
+    event = models.CharField("紀念日", max_length=100)
+
+class TodoList(models.Model):
+    date = models.ForeignKey(Diary, on_delete=models.CASCADE)
+    todo_text = models.CharField(max_length=100)
+    todo_check = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.todo_text
+
+class Asset(models.Model):
+    date = models.ForeignKey(Diary, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+
+class Family(models.Model):
+    date = models.ForeignKey(Diary, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+
+class Suprise(models.Model):
+    date = models.ForeignKey(Diary, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+
+class Relationship(models.Model):
+    date = models.ForeignKey(Diary, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+
+class Study(models.Model):
+    date = models.ForeignKey(Diary, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+
+class Career(models.Model):
+    date = models.ForeignKey(Diary, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+
+class Mind(models.Model):
+    date = models.ForeignKey(Diary, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
